4c7fb3917ec5b3b9e92745f999b221b41809ab55
Alex Babou <ababou@dendev.net>
2021-05-14 14:50:41 -0600

    Initial Commit

---
+++ BrainTrain/BrainTrainApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrainTrainApp.swift
+//  BrainTrain
+//
+//  Created by Alex Babou on 5/14/21.
+//
+
+import SwiftUI
+
+@main
+struct BrainTrainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
